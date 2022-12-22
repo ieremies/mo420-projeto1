326c23d5fd69d6d335550121af936fcc2b3dbd21
Ieremies Romero <ieremies@gmail.com>
2022-12-21 21:10:09 -0300

    final clean up

--- compiler.py
+++ compiler.py
@@ -1,24 +1,18 @@
-
 import os
 import subprocess
 
-directory = 'out'
+directory = "out"
+sep = " ,"
 
 results = os.listdir(directory)
 
 # print("inst", "nnodes", "simpl_it", "time", "work","obj","bound","gap", sep='\t')
 
-for i in sorted(results, key=lambda x: int(x.split("_")[1])):
-    explored = ''
-    best = ''
-    type = i.split()[1]
+for i in sorted(results):
+    explored = ""
+    best = ""
 
-    if type != "e":
-        continue
-
-    with open("out/"+i, "r") as fd:
-        print(" | ", end='')
-        print(i.split()[0][5:], end=' | ')
+    with open("out/" + i, "r") as fd:
         for line in fd:
             if "Explored" in line:
                 explored = line
@@ -32,10 +26,9 @@ for i in sorted(results, key=lambda x: int(x.split("_")[1])):
 
         best = best.split()
         obj = best[2][:-1]
-        obj = float(obj) if obj != '-' else obj
+        obj = float(obj) if obj != "-" else obj
         bound = best[-3][:-1]
-        bound = float(bound) if bound != '-' else bound
+        bound = float(bound) if bound != "-" else bound
         gap = best[-1]
 
-        print(nnodes, time, obj, bound, gap, sep=" | ")
-        print(" | ", end='')
+        print("", i, nnodes, time, obj, bound, gap, "", sep=sep)
--- runner.py
+++ runner.py
@@ -1,18 +1,30 @@
 import os
 import subprocess
+import sys
 
-cmd = "./bin/pli-projeto1.e ./bin/../instances/{instance} {type}"
-directory = 'instances'
+program = sys.argv[1]
+instances_dir = sys.argv[2]
+param = sys.argv[3].split("=")[1].split(",")
 
-instances = os.listdir(directory)
+# TODO use argparse
+print(program, instances_dir, param)
+
+# TODO make before run
+instances = os.listdir(instances_dir)
+
+print(instances)
 
 qtd = 0
-total = len(instances)
-for i in sorted(instances, key=lambda x: int(x.split("_")[1])):
-    type = " l"
-    print(f"executando instância {i} \t({qtd} de {total})")
-    qtd += 1
-    with open("out/"+i+type, "w") as fd:
-        process = subprocess.Popen(cmd.format(instance=i, type=type).split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        fd.write(output.decode())
+total = len(instances) * len(param)
+for i in sorted(instances):
+    for p in param:
+        # TODO tranformar em barra de progresso
+        print(f"executando instância {i} \t({qtd} de {total})")
+        qtd += 1
+        with open(f"out/{i}-{p}.out", "w") as fd:
+            # print([program, i, p])
+            process = subprocess.Popen(
+                [program, f"./{instances_dir}/{i}", p], stdout=subprocess.PIPE
+            )
+            output, error = process.communicate()
+            fd.write(output.decode())
